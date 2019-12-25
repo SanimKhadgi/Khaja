c8e9863b3d297460feed2663d04a6172181f370e
InvI <sanim46.sk@gmail.com>
2019-12-25 23:17:17 +0545

    a

--- app/src/main/java/com/example/khaja/Register.java
+++ app/src/main/java/com/example/khaja/Register.java
@@ -1,4 +1,14 @@
 package com.example.khaja;
 
+import android.os.Bundle;
+
 public class Register {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        etEmail= findViewById(R.id.Email);
+        etPssword=findViewById(R.id.Password);
+        etBtnLogin=findViewById(R.id.BtnLogin);
+        etBtnReg=findViewById(R.id.BtnReg);
 }
