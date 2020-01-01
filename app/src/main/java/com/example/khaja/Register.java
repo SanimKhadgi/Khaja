38c245c6f6f79cc7310a3af6d9ec788836145921
InvI <sanim46.sk@gmail.com>
2020-01-01 20:50:33 +0545

    Edited

--- app/src/main/java/com/example/khaja/Register.java
+++ app/src/main/java/com/example/khaja/Register.java
@@ -4,11 +4,5 @@ import android.os.Bundle;
 
 public class Register {
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
 
-        etEmail= findViewById(R.id.Email);
-        etPssword=findViewById(R.id.Password);
-        etBtnLogin=findViewById(R.id.BtnLogin);
-        etBtnReg=findViewById(R.id.BtnReg);
 };
